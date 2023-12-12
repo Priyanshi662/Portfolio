384658ac592c3d4725197f55c11d25571799ea32
Priyanshi662 <priyanshiw567@gmail.com>
2023-12-12 21:24:47 +0530

    adding projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header"
 import { Content } from "./components/Content"
+import { Projects} from "./components/Project"
 import './App.css'
 function App() {
   return (
     <>
     <Header/>
     <Content/>
+    <Projects/>
     </>
   )
 }
---
+++ src/components/Project.jsx
@@ -0,0 +1,75 @@
+import propTypes from "prop-types";
+import deployed from "../assets/Projects/deployed.png"
+import funfusion from "../assets/Projects/funfusion.png"
+
+export function Projects() {
+  const projects = [
+    {
+      title: "WanderStay",
+      img: deployed,
+      page: "https://wander-stay.netlify.app/",
+      repo: "https://github.com/Priyanshi662/WanderStay",
+      description: "A hospitality exchange service with a seamless user experience, buy and rent rooms all across the globe",
+    },
+    {
+      title: "Fun fusion",
+      img: funfusion,
+      page: "https://juandvp0303.github.io/Tic-Tac-Toe-With-REACT/",
+      repo: "https://github.com/Priyanshi662/FunFusion",
+      description: "Different applications made for entertainment purposes",
+    },
+  ];
+
+  return (
+    <div className="projects-section">
+      <h2>Projects</h2>
+      <section id="projects-container">
+        
+        {projects.map((project,i) => {
+          return <ProjectsCards
+          key={i}
+          title={project.title}
+          img={project.img}
+          page={project.page}
+          repo={project.repo}
+          description={project.description}
+        />
+        
+        })}
+
+      </section>
+    </div>
+  );
+}
+
+function ProjectsCards({ title, img, repo, page, description }) {
+  return (
+    <article className="projects-article">
+      <img className="projects-imgs" src={img} alt="" />
+      <h2>{title}</h2>
+
+      <p className="projects-descriptions">{description}</p>
+
+      <div className="inputs-repository-container">
+        <input
+          type="button"
+          value="Repo link"
+          onClick={() => window.open(repo, "_blank")}
+        />
+        <input
+          type="button"
+          value="Deployed link"
+          onClick={() => window.open(page, "_blank")}
+        />
+      </div>
+    </article>
+  );
+}
+
+ProjectsCards.propTypes = {
+  title: propTypes.string.isRequired,
+  img: propTypes.string.isRequired,
+  repo: propTypes.string.isRequired,
+  page: propTypes.string.isRequired,
+  description: propTypes.string.isRequired,
+};
